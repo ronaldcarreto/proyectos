6014bbcfb387abe99b9157f936fe67e922ab70e2
ronald <2070164610101@ingenieria.usac.edu.gt>
2023-09-29 00:42:20 -0600

    agregar cambios

--- proyectoDjango/proyecto1/proyecto1/views.py
+++ proyectoDjango/proyecto1/proyecto1/views.py
@@ -25,8 +25,13 @@ def pruebaplantilla(request):
 def pruebaplantilla2(request):
     
     nombre="ronald"
+    nombre2="eduardoo"
     apellido="carreto"
     hora=datetime.datetime.now()
+    #clases":["mate","fisica", "electronica"]
+    clases2:[]  #Lo coloque vacio para comprobar una funcion para que coloque un mensaje de que no hay clases disponibles
+
+
 
     doc_externo=open("C:/Users/GEMELOS/Documents/proyectos/proyectoDjango/proyecto1/proyecto1/plantillas/plantilla2.html")
     plt=Template(doc_externo.read())
@@ -34,6 +39,10 @@ def pruebaplantilla2(request):
     ctx=Context({"nombre_persona": nombre, "apellido_persona":apellido, "hora_actual":hora, "clases":["mate","fisica", "electronica"]})
     #ctx=Context({"nombre_persona": "ronald", "apellido_persona":"carreto"})   *otra forma de colocarlo*
     documento=plt.render(ctx)
+
+
+
+
 
     return HttpResponse(documento)
 
